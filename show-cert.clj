f2d1d1ee12f6f76f91524dfdae1084798899ba54
Alexei Matveev <alexei.matveev@gmail.com>
2020-12-11 19:49:00 +0100

    AM: type hints

--- show-cert.clj
+++ show-cert.clj
@@ -3,12 +3,14 @@
 (ns foo.core
   (:require [clojure.java.io :as io]))
 
+;; Type hints seem to be necessary to avoid "Illegal reflective access
+;; by clojure.lang.InjectedInvoker" warnings in recent JDK.
 (defn get-server-certs [from]
   (let [url (io/as-url from)
-        conn (.openConnection url)]
+        ^javax.net.ssl.HttpsURLConnection conn (.openConnection url)]
     (with-open [_ (.getInputStream conn)]
       (.getServerCertificates conn))))
 
 (doseq [url *command-line-args*]
   (println
-   (.getNotAfter (first (get-server-certs url)))))
+   (.getNotAfter ^java.security.cert.X509Certificate (first (get-server-certs url)))))
